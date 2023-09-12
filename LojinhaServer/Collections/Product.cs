fae123dd952f38fe0cdf870bf1dfdda233c5b069
Renan Molina <renanrobertomolina@hotmail.com>
2023-09-12 20:36:48 -0300

    12-09

--- LojinhaServer/Collections/Product.cs
+++ LojinhaServer/Collections/Product.cs
@@ -1,44 +1,29 @@
 using MongoDB.Bson;
-using MongoDB.Bson.Serialzation.Attributes;
+using MongoDB.Bson.Serialization.Attributes;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialzation;
-
+using System.Text.Json.Serialization;
 namespace LojinhaServer.Collections;
-
-    [Table("products")]
-    [BsonIgnoreExtraElements]
-    public class Product
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }  
-
-        [BsonElement("name")]
-         [JsonPropertyName("Nome")]  
-        public string Name { get; set; }
-
-        [BsonElement("Description")]
-        [JsonPropertyName("Descrição")]  
-        public string Description { get; set; }
-
-        [BsonElement("Price")]
-        [JsonPropertyName("Preço")]  
-        public decimal Price { get; set; }
-
-        [BsonElement("OffPrice")]
-        [JsonPropertyName("Desconto")]  
-        public decimal OffPrice { get; set; }
-
-        [BsonElement("Categories")]
-        [JsonPropertyName("Categorias")]  
-        public List<string> Categories { get; set; }
-
-        [BsonElement("Tags")]
-        [JsonPropertyName("Tags")]  
-        public List<string> Tags { get; set; }
-
-        [BsonElement("Brand")]
-        [JsonPropertyName("Marca")]  
-        public string Brand { get; set; }
-
-    }
+[Table("products")]
+[BsonIgnoreExtraElements]
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonElement("name")]
+    [JsonPropertyName("Nome")]
+    public string Name { get; set; }
+    [BsonElement("description")]
+    [JsonPropertyName("Descrição")]
+    public string Description { get; set; }
+    [BsonElement("price")]
+    public decimal Price { get; set; }
+    [BsonElement("offPrice")]
+    public decimal OffPrice { get; set; }
+    [BsonElement("categories")]
+    public List<string> Categories { get; set; }
+    [BsonElement("tags")]
+    public List<string> Tags { get; set; }
+    [BsonElement("brand")]
+    public string Brand { get; set; }
+}
--- LojinhaServer/Extensions/ServiceExtensions.cs
+++ LojinhaServer/Extensions/ServiceExtensions.cs
@@ -1,34 +1,40 @@
 using LojinhaServer.Models;
 using MongoDB.Driver;
+using LojinhaServer.Repositories;
 
 namespace LojinhaServer.Extensions;
 
-    public static class ServiceExtensions
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
     {
-        public static void ConfigureCors(this IServiceCollection services)
+        services.AddCors(options =>
         {
-            services.AddCors(options => {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                );
-            });
-        }
-
-
-        public static void ConfigureMongoDBSettings(this IServiceCollection services, IConfiguration config)
-        {
-            services.Configure<MongoDBStettings>(
-                config.GetSection("MongoDBSettings")
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
             );
-
-            services.AddSingleton<IMongoDatabase>(options =>{
-                var settings = config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
-                var client = new MongoClient(settings.ConnectionString);
-                return client.GetDatabase(settings.DatabaseName);
-            });
-        }
-
-
+        });
     }
+
+
+    public static void ConfigureMongoDBSettings(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<MongoDBSettings>(
+            config.GetSection("MongoDBSettings")
+        );
+
+        services.AddSingleton<IMongoDatabase>(options =>
+        {
+            var settings = config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+            var client = new MongoClient(settings.ConnectionString);
+            return client.GetDatabase(settings.DatabaseName);
+        });
+    }
+    public static void ConfigureProductRepository(this IServiceCollection services)
+    {
+        services.AddSingleton<IProductRepository, ProductRepository>();
+    }
+
+}
